d7c0b03649ea77b2f4c941fa2a288e0d72a7d5f0
hassanShakur <hass.shakurhassan@gmail.com>
2023-01-08 21:52:58 +0300

    useReducer complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // rafce == React ArrowFunction Component Export
 
 import React from 'react';
-import Setup from './tutorial/5-useRef/1-useRef-basics'
+import Setup from './tutorial/6-useReducer'
 function App() {
   return (
     <div className='container'>
